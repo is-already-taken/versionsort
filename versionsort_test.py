1ae1dcfccc4d4c8144735f09d953b18a6c4e2aea
Thomas Lehmann <thomas.lehmann@stueckseln.de>
2013-06-21 22:14:00 +0200

    [WIP] Refactored tests, added more tests

--- versionsort_test.py
+++ versionsort_test.py
@@ -2,46 +2,22 @@ import unittest
 from versionsort import VersionSort
 
 class DefaultWidgetSizeTestCase(unittest.TestCase):
-	unsorted_lines = [
-		'string_with/any_char/1.0-12',
-		'string_with/any_char/1.1-2',
-		'string_with/any_char/1.1-24',
-		'string_with/any_char/0.9-563',
-		'string_with/any_char/1.1.1-5',
-		'string_with/any_char/1.3-2',
-		'string_with/any_char/1.3-9'
+	unsorted_versions = [
+		'1.0-12', '1.1-2', '1.1-24', '0.9-563', '1.1.1-5', '1.3-2', '1.3-9'
 	]
 
-	expect_sorted_lines = [
-		'string_with/any_char/1.3-9',
-		'string_with/any_char/1.3-2',
-		'string_with/any_char/1.1.1-5',
-		'string_with/any_char/1.1-24',
-		'string_with/any_char/1.1-2',
-		'string_with/any_char/1.0-12',
-		'string_with/any_char/0.9-563'
+	expect_sorted_versions = [
+		'1.3-9', '1.3-2', '1.1.1-5', '1.1-24', '1.1-2', '1.0-12', '0.9-563'
 	]
 
-	unsorted_lines_2 = [
-		'version strings may be 1.0-12 contained in between too',
-		'version strings may be 1.1-2 contained in between too',
-		'version strings may be 1.1-24 contained in between too',
-		'version strings may be 0.9-563 contained in between too',
-		'version strings may be 1.1.1-5 contained in between too',
-		'version strings may be 1.3-2 contained in between too',
-		'version strings may be 1.3-9 contained in between too'
-	]
 
-	expect_sorted_lines_2 = [
-		'version strings may be 1.3-9 contained in between too',
-		'version strings may be 1.3-2 contained in between too',
-		'version strings may be 1.1.1-5 contained in between too',
-		'version strings may be 1.1-24 contained in between too',
-		'version strings may be 1.1-2 contained in between too',
-		'version strings may be 1.0-12 contained in between too',
-		'version strings may be 0.9-563 contained in between too'
-	]
+	def generate_lines(self, string, version_list):
+		lines = []
+		for version in version_list:
+			lines.append(string.format(version))
 
+		return lines
+		
 
 	def assertions(self, sorted_lines, expected_sorted_lines):
 		self.assertEqual(len(sorted_lines), len(expected_sorted_lines), 'Count does not equal, actual value \'{0}\' expected value \'{1}\''.format(len(sorted_lines),  len(expected_sorted_lines)))
@@ -56,21 +32,43 @@ class DefaultWidgetSizeTestCase(unittest.TestCase):
 	def setUp(self):
 		self.sorter = VersionSort()
 
+
+
+
 	def testDefaultSortSignature(self):
-	 	sorted_lines = self.sorter.sort(lines=self.unsorted_lines, reverse=True)
-		self.assertions(sorted_lines, self.expect_sorted_lines)
+		unsorted_lines = self.generate_lines('version strings may be {0} contained in between too', self.unsorted_versions)
+		sorted_lines_expect = self.generate_lines('version strings may be {0} contained in between too', self.expect_sorted_versions)
 
-		expect_sorted_lines_2_reverse = []
-		expect_sorted_lines_2_reverse.extend(self.expect_sorted_lines_2)
-		expect_sorted_lines_2_reverse.reverse()
+	 	sorted_lines = self.sorter.sort(lines=unsorted_lines, reverse=True)
 
-	 	sorted_lines = self.sorter.sort(lines=self.unsorted_lines_2, version_field_matcher='(\d+(:?\.\d+)+(:?-\d+)?)', reverse=False)
-		self.assertions(sorted_lines, expect_sorted_lines_2_reverse)
+		self.assertions(sorted_lines, sorted_lines_expect)
 
-		
+
+	def testSepAndFieldNum(self):
+		unsorted_lines = self.generate_lines('version;is;at;{0};field;four;separated;by;semicolon', self.unsorted_versions)
+		sorted_lines_expect = self.generate_lines('version;is;at;{0};field;four;separated;by;semicolon', self.expect_sorted_versions)
+
+	 	sorted_lines = self.sorter.sort(lines=unsorted_lines, field_sep=";", field_num=4, reverse=True)
+
+		self.assertions(sorted_lines, sorted_lines_expect)
+
+
+
+	def testReverse(self):
+		unsorted_lines = self.generate_lines('string_with/any_char/{0}', self.unsorted_versions)
+		sorted_lines_expect = self.generate_lines('string_with/any_char/{0}', self.expect_sorted_versions)
+		sorted_lines_expect.reverse()
+
+	 	sorted_lines = self.sorter.sort(lines=unsorted_lines, reverse=False)
+
+		self.assertions(sorted_lines, sorted_lines_expect)
 
 
 	def testCustomMatcher(self):
-		sorted_lines = self.sorter.sort(self.unsorted_lines, '^.*/([^/]+)$', '-', reverse=True)
-		self.assertions(sorted_lines, self.expect_sorted_lines)
+		unsorted_lines = self.generate_lines('string_with/any_char/{0}', self.unsorted_versions)
+		sorted_lines_expect = self.generate_lines('string_with/any_char/{0}', self.expect_sorted_versions)
+
+	 	sorted_lines = self.sorter.sort(lines=unsorted_lines, version_field_matcher='^.*/([^/]+)$', reverse=True)
+
+		self.assertions(sorted_lines, sorted_lines_expect)
 
